52e924b411868f10a4558b82ca1d40248c414d8d
Christopher Lamm <chris@theantichris.com>
2019-12-12 15:30:51 -0500

    switched output to text template

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"github.com/theantichris/github"
 	"log"
 	"os"
+	"text/template"
+	"time"
+
+	"github.com/theantichris/github"
 )
 
+const issueListTemplate = `{{.TotalCount}} issues:
+{{range .Items}}-------------------------------------
+Number: {{.Number}}
+User:	{{.User.Login}}
+Title:	{{.Title | printf "%.64s"}}
+Age:	{{.CreatedAt | daysAgo}} days
+{{end}}`
+
+var issueList = template.Must(template.New("issueList").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(issueListTemplate))
+
 func main() {
-	result, err := github.SearchIssues(os.Args[1:])
+	issues, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	if err := issueList.Execute(os.Stdout, issues); err != nil {
+		log.Fatal(err)
 	}
+}
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
 }
